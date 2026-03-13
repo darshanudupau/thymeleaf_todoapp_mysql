e2c617048525166fd75d2d7500709c927b1ff83d
darshanudupau <36585454+darshanudupau@users.noreply.github.com>
2026-03-13 04:56:15 -0700

    The TODO app with mysql and thymeleaf
    The connection issue of the mysql still exists. 
    
    Logs:-
    
    Logs:-
    Caused by: java.net.ConnectException: Connection refused: getsockopt
            at java.base/sun.nio.ch.Net.pollConnect(Native Method) ~[na:na]
            at java.base/sun.nio.ch.Net.pollConnectNow(Net.java:690) ~[na:na]
            at java.base/sun.nio.ch.NioSocketImpl.timedFinishConnect(NioSocketImpl.java:542) ~[na:na]        
            at java.base/sun.nio.ch.NioSocketImpl.connect(NioSocketImpl.java:592) ~[na:na]
            at java.base/java.net.SocksSocketImpl.connect(SocksSocketImpl.java:327) ~[na:na]
            at java.base/java.net.Socket.connect(Socket.java:751) ~[na:na]
            at com.mysql.cj.protocol.StandardSocketFactory.connect(StandardSocketFactory.java:144) ~[mysql-connector-j-9.6.0.jar:9.6.0]
            at com.mysql.cj.protocol.a.NativeSocketConnection.connect(NativeSocketConnection.java:53) ~[mysql-connector-j-9.6.0.jar:9.6.0]
            ... 58 common frames omitted

---
+++ todo/src/main/java/com/app/todo/services/TaskService.java
@@ -0,0 +1,46 @@
+package com.app.todo.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.app.todo.models.Task;
+import com.app.todo.repository.TaskRepository;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void createTask(String title){
+        Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+
+    }
+
+    public void toggleTask(Long id) {
+    Task task = taskRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid task id"));
+    task.setCompleted(!task.isCompleted());
+    taskRepository.save(task);
+}
+
+    
+
+    
+}
